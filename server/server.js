530d24332854235e3b11f8c1ab3383ce019bcfd3
Ashish Gola <golaashish036@gmail.com>
2025-08-28 01:25:19 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
-  res.send('API is Working');
+  res.send("API is Working");
 });
 
 const PORT = process.env.PORT || 3000;
@@ -17,5 +18,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
-export default app;
